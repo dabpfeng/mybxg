f516a1b687ad21a1715514c1f52722f08ec3df6f
dabpfeng <316665396@qq.com>
2017-09-24 20:47:27 +0800

    完成个人中心表单提交功能[C

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,5 @@
-define(['jquery','template','ckeditor','uploadify','region','datepicker','language'], function($, template,CKEDITOR) {
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language',
+'validate','form'], function($, template,CKEDITOR) {
     // 调用借口 获取个人信息
     $.ajax({
         type : 'get',
@@ -36,8 +37,35 @@ define(['jquery','template','ckeditor','uploadify','region','datepicker','langua
                     { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
                     '/',
                 ]
-            
             });
+
+            // 处理表单提交
+            $('#settingsForm').validate({
+                sendForm : false,
+                valid : function(){
+                    // 获取家乡信息
+                    var p = $('#p').find('option:selected').text();
+                    var c = $('#c').find('option:selected').text();
+                    var d = $('#d').find('option:selected').text();
+                    var hometown = p + '|' + c + '|' + d;
+                    // 同比不富文本内容
+                    for (var instance in CKEDITOR.instances) {  // instance  实例
+                        CKEDITOR.instances[instance].updateElement(); // 更新元素
+                    }
+                    $(this).ajaxSubmit({
+                        type : 'post',
+                        url : '/api/teacher/modify',
+                        data : {tc_hometown : hometown},
+                        dataType : 'json',
+                        success : function(data) {
+                            // 修改成功之后　重载当前页面
+                            // location.reload();
+                            // 更改富文本信息，值修改内容保存的话内容不会更改，应该该内容是更改了p标签里的内容  p在iframe中,
+                            // 而原来的内容是在textarea里面
+                        }
+                    })
+                }
+            })
         }
     })
 });
