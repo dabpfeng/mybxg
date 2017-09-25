20f956a5238c8a96468dc9afb8bb98f089ff9dbd
dabpfeng <316665396@qq.com>
2017-09-25 09:50:35 +0800

    完成列表展示功能

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -1,4 +1,16 @@
 define(['jquery','template','util'],function($,template,util) {
     // 设置导航菜单选中
     util.setMenu(location.pathname);
+    // 课程列表渲染
+    $.ajax({
+        type : 'get',
+        url : '/api/course',
+        dataType: 'json',
+        success : function(data) {
+            // console.log(data);
+            //解析数据 渲染页面
+            var html = template('courselistTpl',{list: data.result});
+            $('#courseInfo').html(html);
+        }
+    })
 })
