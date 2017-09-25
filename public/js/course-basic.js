59366036ee11dd9a80b662a60fff40839947e233
dabpfeng <316665396@qq.com>
2017-09-25 21:56:43 +0800

    完成课程基本信息提交功能

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','ckeditor'],function($,template,util,CKEDITOR) {
+define(['jquery','template','util','ckeditor','validate','form'],function($,template,util,CKEDITOR) {
     //设置导航菜单选中
     util.setMenu('/course/add');
     //获取课程ID
@@ -50,6 +50,30 @@ define(['jquery','template','util','ckeditor'],function($,template,util,CKEDITOR
                     '/',
                 ]
             });
+
+            // 处理表单提交
+            $("#basicForm").validate({
+                sendForm : false,
+                valid : function(){
+                    // 处理富文本同步
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    // 表单提交
+                    $(this).ajaxSubmit({
+                        type : 'post',
+                        url : '/api/course/update/basic',
+                        data: {cs_id : csId},
+                        dataType : 'json',
+                        success : function(data){
+                            console.log(data);
+                            if(data.code == 200) {
+                                location.href = '/course/picture?cs_id' + data.result.cs_id;
+                            }
+                        }
+                    })
+                }
+            })
         }
     })
 });
